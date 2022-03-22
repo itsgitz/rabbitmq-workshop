12627ef412e90b137e76dc9ade2eb8dee8134eb4
itsgitz <anggit.ginanjar@outlook.com>
2022-03-23 06:56:25 +0700

    feat: Durable and ACK

--- php/getstarted/new_tasks.php
+++ php/getstarted/new_tasks.php
@@ -5,6 +5,8 @@ require './vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+$queueName = 'proxmox_nodes_repair';
+
 $connection = new AMQPStreamConnection(
     'localhost',
     5672,
@@ -13,6 +15,13 @@ $connection = new AMQPStreamConnection(
 );
 
 $channel = $connection->channel();
+$channel->queue_declare(
+    $queueName,
+    false,
+    true, // durable
+    false,
+    false
+);
 
 $data = implode(' ', array_slice($argv, 1));
 
@@ -20,9 +29,12 @@ if (empty($data)) {
     $data = 'Hello World';
 }
 
-$msg = new AMQPMessage($data);
+$msg = new AMQPMessage(
+    $data,
+    ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]
+);
 
-$channel->basic_publish($msg, '', 'hello');
+$channel->basic_publish($msg, '', $queueName);
 
 echo ' Sent ' . $data . PHP_EOL;
 
--- php/getstarted/worker.php
+++ php/getstarted/worker.php
@@ -4,6 +4,8 @@ require './vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
+$queueName = 'proxmox_nodes_repair';
+
 $connection = new AMQPStreamConnection(
     'localhost',
     5672,
@@ -13,7 +15,13 @@ $connection = new AMQPStreamConnection(
 
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);
+$channel->queue_declare(
+    $queueName,
+    false,
+    true, // durable
+    false,
+    false
+);
 
 echo 'Waiting for message ...' . PHP_EOL;
 
@@ -25,10 +33,20 @@ $callback = function ($msg) {
     echo $msg->body . PHP_EOL;
     echo 'Done' . PHP_EOL;
 
-    $msg->ack;
+    $msg->ack();
 };
 
-$channel->basic_consume('proxmox_node_repair_data_collection', '', false, false, false, false, $callback);
+$channel->basic_qos(null, 1, null);
+
+$channel->basic_consume(
+    $queueName,
+    '',
+    false,
+    false, // true means no ACK
+    false,
+    false,
+    $callback
+);
 
 while ($channel->is_open()) {
     $channel->wait();
